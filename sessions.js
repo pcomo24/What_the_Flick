28da8a01e5f97438ef31ae551323b994a55cb93a
JJ <jj.spetseris@yahoo.com>
2017-06-23 12:11:02 -0500

    api calls are working and Movies returns correct range

--- sessions.js
+++ sessions.js
@@ -1,4 +1,4 @@
-function Movies(request) {
+function Movies(request, pageLimit) {
   request.session.nextMovie;
 //array which stores composite numeric values for previous movie lookups
   request.session.movies = [];
@@ -12,7 +12,7 @@ function Movies(request) {
 //present in "themoviedb.org" database api. The values are checked to be unique,
 //-and then returned as a (2) element array.
   request.newMovie = function () {
-    request.session.nextMoviePage = Math.ceil(Math.random() * 1000);
+    request.session.nextMoviePage = Math.ceil(Math.random() * pageLimit);
     request.session.nextMovieSelection = Math.floor(Math.random() * 20);
 //numeric variable request.session.nextMovieSelection is divided by 100 and added to integer request.session.nextMoviePage so
 //-that both variables can be stored as (1) floating point numeric value for crosss-checking efficiency.
