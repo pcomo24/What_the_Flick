10e3e487f2f20b8d293ca7f95b8f6401c43388d4
JJ <jj.spetseris@yahoo.com>
2017-06-27 10:03:06 -0500

    added hint limits

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,28 @@
+// track how many hints the user has seen
+var hintCount = 0 ;
+
 function getHint(overviewHint) {
+  // controls clicking of the hint button
   var hint = document.getElementById('hint');
-  if (hint.innerHTML == '') {
-    hint.innerHTML = overviewHint;
+  // checks if hint count is less than 3 and displays hint
+  if (hintCount < 3) {
+    if (hint.innerHTML == '') {
+      hint.innerHTML = overviewHint;
+      hintCount++;
+    // user can close hint with a second click
+    } else {
+      hint.innerHTML = '';
+    }
+  // checks if hint count is over 3 and displays message
   } else {
-    hint.innerHTML = '';
+    if (hint.innerHTML == '') {
+      hint.innerHTML = 'You already used up your 3 hints.';
+    // user can close hint with a second click
+    } else {
+      hint.innerHTML = '';
+    }
   }
 }
+
+var gameButton = document.getElementById('newGame-button');
+gameButton.href = '/?ts=' + Date.now();
